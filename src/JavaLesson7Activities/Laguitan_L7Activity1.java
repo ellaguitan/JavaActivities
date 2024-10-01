607100182ecc329bacda93a0be8a6e4ba111123f
ellaguitan <el.laguitan@awsys-i.com>
2024-10-01 16:30:19 +0800

    Added Activity 2

--- src/JavaLesson7Activities/Laguitan_L7Activity1.java
+++ src/JavaLesson7Activities/Laguitan_L7Activity1.java
@@ -6,19 +6,26 @@ public class Laguitan_L7Activity1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Passenger passenger = new Passenger();
 		
+		// Lesson 2 Activity 2
+		Passenger passenger = new Passenger("Viktor Magtatanggol", 'M', "Canadian", "June 12, 1898");
+		
+		Passenger passenger1 = new Passenger("Vicky Magtatanggol", 'F', "Canadian", "June 12, 1898", 1234567890);
+		
+		// Lesson 7 Activity 1
+//		Passenger passenger = new Passenger();
+//		
 //		passenger.name = "Viktor Magtatanggol";
 //		passenger.gender = 'M';
 //		passenger.nationality = "Canadian";
 //		passenger.dateOfBirth = "June 12, 1898";
 //		passenger.passport = new String[] {passenger.name, passenger.nationality, passenger.dateOfBirth};
-		
-		System.out.println(passenger.name);
-		System.out.println(passenger.gender);
-		System.out.println(passenger.nationality);
-		System.out.println(passenger.dateOfBirth);
-		System.out.println(Arrays.toString(passenger.passport));
+//		
+//		System.out.println(passenger.name);
+//		System.out.println(passenger.gender);
+//		System.out.println(passenger.nationality);
+//		System.out.println(passenger.dateOfBirth);
+//		System.out.println(Arrays.toString(passenger.passport));
 	}
 
 }
--- src/JavaLesson7Activities/Passenger.java
+++ src/JavaLesson7Activities/Passenger.java
@@ -1,5 +1,7 @@
 package JavaLesson7Activities;
 
+import java.util.Arrays;
+
 class Passenger {
 	public String name;
 	public char gender;
@@ -7,25 +9,32 @@ class Passenger {
 	public String dateOfBirth;
 	public String[] passport;
 	
-	public Passenger() {
-		name = "";
-		gender = ' ';
-		nationality = "";
-		dateOfBirth = "";
-		passport = new String[] {name, nationality, dateOfBirth};
-		
-		System.out.println("New passenger created.");
-	}
+	// Lesson 7 Activity 1
+//	public Passenger(String name, char gender, String nationality, String dateOfBirth, String[] passport) {
+//		this.name = name;
+//		this.gender = gender;
+//		this.nationality = nationality;
+//		this.dateOfBirth = dateOfBirth;
+//		this.passport = passport;
+//	}
 	
-	public Passenger(String name, char gender, String nationality, String dateOfBirth, String[] passport) {
+	// Lesson 7 Activity 2
+	public Passenger(String name, char gender, String nationality, String dateOfBirth) {
 		this.name = name;
 		this.gender = gender;
 		this.nationality = nationality;
 		this.dateOfBirth = dateOfBirth;
-		this.passport = passport;
+		
+		System.out.println(this.toString());
 	}
 	
-	public static String getName() {
+	public Passenger(String name, char gender, String nationality, String dateOfBirth, long pwdIDNumber) {
+		this(name, gender, nationality, dateOfBirth);
+		System.out.println("PWD ID No.: " + pwdIDNumber);
+	}
+	//
+	
+	public String getName() {
 		return name;
 	}
 	
@@ -64,5 +73,12 @@ class Passenger {
 	public void setPassport(String[] passport) {
 		this.passport = passport;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "\nName: " + name + 
+			   "\nGender: " + gender + 
+			   "\nNationality: " + nationality + 
+			   "\nDate Of Birth: " + dateOfBirth;
+	}
 }
