e8ec150c1520734cf3baa1bc3cad1a1a26df6efa
ellaguitan <el.laguitan@awsys-i.com>
2024-11-05 19:32:20 +0800

    added Exception Handling to L4 Activity

---
+++ src/Laguitan_L4Activity1/OutOfBoundDigitsException.java
@@ -0,0 +1,21 @@
+package Laguitan_L4Activity1;
+
+public class OutOfBoundDigitsException extends Exception {
+	public OutOfBoundDigitsException(String message) {
+		super(message);
+	}
+	
+	public static void validateDigits(long number) throws OutOfBoundDigitsException{
+		String str  = String.valueOf(number);
+		int str_length = str.length();
+		boolean isValid = false;
+		
+		if (str_length == 10) {
+			isValid = true;
+		}
+		
+		if (!isValid) {
+			throw new OutOfBoundDigitsException("Invalid phone number! Only 10-digit numbers are allowed.");
+		}
+	}
+}
--- src/Laguitan_L4Activity1/Phonebook.java
+++ src/Laguitan_L4Activity1/Phonebook.java
@@ -4,53 +4,69 @@ import java.util.Scanner;
 
 public class Phonebook {
 
-	public static void main(String[] args) {
-		final int ARRAY_LENGTH = 5;
-		int temp = 0;
-		String pName = "";
-		String pNumber = "";
-		String[] name = new String[ARRAY_LENGTH];
-		String[] phoneNumbers = new String[ARRAY_LENGTH];
-		Scanner scn = new Scanner(System.in);
-		
-		while (temp < ARRAY_LENGTH) {
-			System.out.print("Input Name #" + (temp + 1) + ": ");
-			pName = scn.nextLine();
-			if (pName.equalsIgnoreCase("n/a")) {
-				System.out.println();
-				break;
-			} else {
-				name[temp] = pName;
-			}
-			System.out.print("Input Phone Number of Name #" + (temp + 1) + ": ");
-			pNumber = scn.nextLine();
-			phoneNumbers[temp] = pNumber;
-			
-			System.out.println();
-			temp++;
-		}
-		
-		for (int i = 0; i < ARRAY_LENGTH; i++) {
-			System.out.println("[" + (i + 1) + "]" + name[i]);
-		}
-		
-		System.out.print("\nSearch a name: ");
-		String sName = scn.nextLine();
-		
-		for(int i = 0; i <= ARRAY_LENGTH; i++) {
-			if (i == ARRAY_LENGTH || name[i] == null) {
-				System.out.println("Contact does not exist.");
-				break;
-			} else {
-				if (name[i].equalsIgnoreCase(sName)) {
-					System.out.println("\nName: " + name[i]);
-					System.out.println("Phone Number: " + phoneNumbers[i]);
-					break;
-				}
-			}
-		}
-		
-		scn.close();
-	}
+    public static void main(String[] args) {
+        final int ARRAY_LENGTH = 5;
+        int temp = 0;
+        String pName = "";
+        long pNumber = 0;
+        String[] name = new String[ARRAY_LENGTH];
+        long[] phoneNumbers = new long[ARRAY_LENGTH];
+        Scanner scn = new Scanner(System.in);
 
+        while (temp < ARRAY_LENGTH) {
+            while (true) {
+            	System.out.print("\nInput Name #" + (temp + 1) + ": ");
+                pName = scn.nextLine();
+                if (pName.equalsIgnoreCase("n/a")) {
+                    System.out.println();
+                    break;
+                } else {
+                    name[temp] = pName;
+                }
+                System.out.print("Input Phone Number of Name #" + (temp + 1) + ": ");
+                try {
+                    pNumber = Long.parseLong(scn.nextLine());
+                    OutOfBoundDigitsException.validateDigits(pNumber);
+                    phoneNumbers[temp] = pNumber;
+                    break; // Exit loop if input is valid
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input! Only whole numbers are allowed.");
+                } catch (OutOfBoundDigitsException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            if (pName.equalsIgnoreCase("n/a")) {
+                break;
+            }
+
+            System.out.println();
+            temp++;
+        }
+
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
+        	if (name[i] == null) {
+        		break;
+        	}
+            System.out.println("[" + (i + 1) + "]" + name[i]);
+        }
+        
+        System.out.print("\nSearch a name: ");
+        String sName = scn.nextLine();
+
+        for (int i = 0; i <= ARRAY_LENGTH; i++) {
+            if (i == ARRAY_LENGTH || name[i] == null) {
+                System.out.println("Contact does not exist.");
+                break;
+            } else {
+                if (name[i].equalsIgnoreCase(sName)) {
+                    System.out.println("\nName: " + name[i]);
+                    System.out.println("Phone Number: " + phoneNumbers[i]);
+                    break;
+                }
+            }
+        }
+
+        scn.close();
+    }
 }
